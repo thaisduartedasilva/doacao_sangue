23ba2a3a2af96eee2b9f7458d11fb86a675ad7ba
thaisduarte <thais_duarte-silva@estudante.sesisenai.org.br>
2026-04-10 11:57:44 -0300

    add - js

--- javaScript.js
+++ javaScript.js
@@ -1,4 +1,13 @@
 document.getElementById("formulario").addEventListener("submit", function (e) {
     e.preventDefault();
-    
+
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let idade = document.getElementById("idade").value;
+    let peso = document.getElementById("peso").value;
+    let telefone = document.getElementById("telefone").value;
+    let tipoSanguineo = document.getElementById("tipoSanguineo").value
+    let cidade = document.getElementById("cidade").value;
+    let estado = document.getElementById("estado").value;
+
 });
