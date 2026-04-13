e26eb3873c8a4de03a317d49c4fd56a063313b59
thaisduarte <thais_duarte-silva@estudante.sesisenai.org.br>
2026-04-13 11:45:56 -0300

    add - validação formulário

--- javaScript.js
+++ javaScript.js
@@ -1,51 +1,105 @@
+let dados = [];
 document.getElementById("formulario").addEventListener("submit", function (e) {
     e.preventDefault();
-    console.log("hey!");
 
-    let nome = document.getElementById("nome").value;
-    let sobrenome = document.getElementById("sobrenome").value;
-    let email = document.getElementById("email").value;
-    let idade = document.getElementById("idade").value;
-    let peso = document.getElementById("peso").value;
-    let telefone = document.getElementById("telefone").value;
-    let tipoSanguineo = document.getElementById("tipoSanguineo").value
-    let cidade = document.getElementById("cidade").value;
-    let estado = document.getElementById("estado").value;
+    function validarFormulario() {
 
-    if (nome === "") return alert("Campo obigatório!");
-    if (sobrenome === "") return alert("Campo obigatório!");
-    if (email === "") return alert("Campo obigatório!");
-    if (idade === "") return alert("Campo obigatório!");
-    if (peso === "") return alert("Campo obigatório!");
-    if (telefone === "") return alert("Campo obigatório!");
-    if (cidade === "") return alert("Campo obigatório!");
-    if (estado === "") return alert("Campo obigatório!");
-    if (tipoSanguineo === "") return alert("Selecione um tipo sanguineo!");
+        let nome = document.getElementById("nome").value;
+        let sobrenome = document.getElementById("sobrenome").value;
+        let email = document.getElementById("email").value;
+        let idade = document.getElementById("idade").value;
+        let peso = document.getElementById("peso").value;
+        let telefone = document.getElementById("telefone").value;
+        let tipoSanguineo = document.getElementById("tipoSanguineo").value
+        let cidade = document.getElementById("cidade").value;
+        let estado = document.getElementById("estado").value;
 
-    if (nome.length < 3) return alert("Nome inválido");
-    if (sobrenome.length < 3) return alert("Sobrenome inválido");
-    if (!email.includes("@")) return alert("Email inválido!");
-    if (telefone.length < 8) return alert("Número de telefone inválido!");
-    if (isNaN(telefone)) return alert("Informe um número de telefone válido!");
-    if (idade < 16) return alert("Você ainda é muito novo para doar sangue!");
-    if (peso < 50) return alert("Você precida ter mais de 50kg para poder doar sangue!");
+        if (nome === ""){
+            alert("Campo obigatório!");
+            return false;
+        } 
+        if (sobrenome === ""){
+            alert("Campo obigatório!");
+            return false;
+        }
+        if (email === ""){
+            alert("Campo obigatório!");
+            return false;
+        } 
+        if (idade === ""){
+           alert("Campo obigatório!");
+            return false;
+        }
+        if (peso === ""){
+            alert("Campo obigatório!");
+            return false;
+        }
+        if (telefone === ""){
+            alert("Campo obigatório!");
+            return false;
+        }
+        if (cidade === ""){
+            alert("Campo obigatório!");
+            return false;
+        }
+        if (estado === ""){
+            alert("Campo obigatório!");
+            return false;
+        }
+        if (tipoSanguineo === ""){
+            alert("Selecione um tipo sanguineo!");
+            return false;
+        }
 
-    let cadastro = {
-        nome,
-        sobrenome,
-        email,
-        idade,
-        peso,
-        tipoSanguineo,
-        telefone,
-        cidade,
-        estado
-    };
+        if (nome.length < 3) {
+            alert("Nome inválido");
+            return false;
+        }
+        if (sobrenome.length < 3){
+            alert("Sobrenome inválido");
+            return false;
+        }
+        if (!email.includes("@")){
+            alert("Email inválido!");
+            return false;
+        }
+        if (telefone.length < 8) {
+            alert("Número de telefone inválido!");
+            return false;
+        }
+        if (isNaN(telefone)) {
+            alert("Informe um número de telefone válido!");
+            return false;
+        }
+        if (idade < 16) {
+            alert("Você ainda é muito novo para doar sangue!");
+            return false;
+        }
+        if (peso < 50) {
+            alert("Você precida ter mais de 50kg para poder doar sangue!");
+            return false;
+        }
 
-    dados.push(cadastro);
+        let cadastro = {
+            nome,
+            sobrenome,
+            email,
+            idade,
+            peso,
+            tipoSanguineo,
+            telefone,
+            cidade,
+            estado
+        };
 
-    document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso ! <br>" + "Total cadastrados: " + dados.length;
+        dados.push(cadastro);
 
-    console.log(dados);
+        console.log(dados);
 
+            document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso ! <br>" + "Total cadastrados: " + dados.length;
+
+        return true;
+
+    }
+        validarFormulario();
 });
